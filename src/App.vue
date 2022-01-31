77ba598a709914063e649bbb476379f630283406
dominic-soh <dominic.doel.soh.jun.wei@gmail.com>
2022-01-31 15:40:37 +0900

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Let's gooooooooo"/>
+  <div id='app'>
+    <todo></todo>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import todo from './components/todo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    todo
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/todo.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <h2 class="text-center mt-5">This is a freaking Todo app</h2>
+    <!-- Input -->
+    <div class="d-flex">
+      <input v-model="task" placeholder="Enter Task" class="form-control" />
+      <button @click="submitTask" class="btn btn-warning rounded-0">
+        Submit
+      </button>
+    </div>
+    <!-- Table -->
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>{{ task.Task }}</td>
+          <td class="pointer" v-on:click="changeStatus(index)">{{ task.Status }}</td>
+          <td>
+            <div class="text-center" @click="deleteTask(index)">
+              <span class="fa fa-trash"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todo",
+  props: {
+    msg: String,
+  },
+
+  data() {
+    return {
+      task: "",
+      statuses: ["To-do", "In progress", "Completed"],
+      tasks: [
+        {
+          Task: "Jihad",
+          Status: "To-do",
+        },
+        {
+          Task: "Visit Masjid",
+          Status: "In progress",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    submitTask() {
+      if (this.task.length === 0) return;
+
+      this.tasks.push({
+        Task: this.task,
+        Status: "To-do",
+      });
+
+      this.task = "";
+    },
+
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+  },
+
+  editTask() {
+    this.tasks.edit = !this.tasks.edit;
+  },
+
+  changeStatus(index) {
+    console.log(index)
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.pointer{
+  cursor: pointer;
+}
+</style>
