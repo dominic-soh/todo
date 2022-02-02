53a9b74f221c12c3cb07f2f6eb294e681431ca67
Dominic Soh <dominic.doel.soh.jun.wei@gmail.com>
2022-02-02 18:21:11 +0900

    deployable

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# abort on errors
+
+set -e
+
+# build
+
+npm run build
+
+cd dist
+
+git init
+
+git add -A 
+
+git commit -m 'deploy'
+
+git push -f git@github.com:dominic-soh/todo.git master:gh-pages
+
+cd -
